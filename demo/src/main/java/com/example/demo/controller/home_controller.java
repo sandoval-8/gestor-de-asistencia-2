7975736552cb702564c6231b408a144a9e3b0db1
sandoval lucas <sandovalucas88@gmail.com>
2020-11-30 13:22:19 -0300

    agregue mapstruct

--- demo/src/main/java/com/example/demo/controller/home_controller.java
+++ demo/src/main/java/com/example/demo/controller/home_controller.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.demo.model.person;
+import com.example.demo.DTO.personDTO;
 import com.example.demo.service.personService;
 
 @Controller
@@ -18,7 +18,7 @@ public class home_controller {
 	
 	@GetMapping("/")
 	public String index(Model modelo) {
-		List<person> personas = servicio.personAll();
+		List<personDTO> personas = servicio.personAll();
 		modelo.addAttribute("personas", personas);
 		return "listaTabla";
 	}
