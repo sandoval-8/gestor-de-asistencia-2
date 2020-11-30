87908d0f2150d700e1d67e41b467536d2ce289c4
sandoval lucas <sandovalucas88@gmail.com>
2020-11-30 13:24:10 -0300

    entidad mapeando tabla 'person'

--- demo/src/main/java/com/example/demo/model/person.java
+++ demo/src/main/java/com/example/demo/model/person.java
@@ -22,6 +22,53 @@ public class person {
 	private String horario;
 	private String legajo;
 	
-	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getFuncion() {
+		return funcion;
+	}
+	public void setFuncion(String funcion) {
+		this.funcion = funcion;
+	}
+	public String getDomicilio() {
+		return domicilio;
+	}
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+	public String getOficina() {
+		return oficina;
+	}
+	public void setOficina(String oficina) {
+		this.oficina = oficina;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	public String getLegajo() {
+		return legajo;
+	}
+	public void setLegajo(String legajo) {
+		this.legajo = legajo;
+	}	
 
 }
